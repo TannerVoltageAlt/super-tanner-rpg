aa5272274e7637c5544344da9b0a5145be058042
TannerVoltageAlt <159682226+TannerVoltageAlt@users.noreply.github.com>
2024-05-11 10:17:36 -0700

    Update pxt.json, main.blocks, main.ts, tilemap.g.jres, tilemap.g.ts

--- main.ts
+++ main.ts
@@ -1,3 +1,4 @@
 let mySprite = sprites.create(assets.image`playerSprite`, SpriteKind.Player)
 controller.moveSprite(mySprite)
 mySprite.setStayInScreen(true)
+tiles.setCurrentTilemap(tilemap`level2`)
